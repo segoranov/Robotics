b60e338496a92bc9332a94505eedfcecdbba6253
Stiliyan Goranov <segoranov@gmail.com>
2021-04-19 14:27:41 +0300

    Update robot interface and move files to separate directory

--- esp32-wroover-motor-driver-pwm/robot-motors-pwm/Robot.h
+++ esp32-wroover-motor-driver-pwm/robot-motors-pwm/Robot.h
@@ -8,7 +8,11 @@ class Robot
   public:
     virtual void moveForward(int speed) = 0;
     virtual void moveBackward(int speed) = 0;
+    virtual void turnRight(int degrees) = 0;
+    virtual void turnLeft(int degrees) = 0;
     virtual void stopMoving() = 0;
+    virtual bool isMoving() = 0;
+    virtual bool isNotMoving() = 0;
 };
 
 // For slow movement, good speed is 110
@@ -27,9 +31,30 @@ class RobotImpl : public Robot
       setMotorsSpeed(speed);
     }
 
+    virtual void turnRight(int degrees) override
+    {
+
+    }
+
+    virtual void turnLeft(int degrees) override
+    {
+
+    }
+
     virtual void stopMoving() override
     {
       turnMotorsOff();
+    }
+
+    virtual bool isMoving() override
+    {
+      // TODO
+      return false;
+    }
+
+    virtual bool isNotMoving() override
+    {
+      return !isMoving();
     }
 
   private:
